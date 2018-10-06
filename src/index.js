a4be9ab6126b2c4672d9d5dd171387815134fa80
gulyhuli <42315248+gulyhuli@users.noreply.github.com>
2018-10-06 15:49:47 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ bot.onText(/^\/[a-zA-Z]+$/, msg => {
     case '/start':
     case '/help':
   bot.sendMessage(id, helper.description).then(() => {
-        bot.sendMessage(id, helper.description)
+         bot.sendMessage(id, helper.description),
 		 return bot.sendMessage(id, helper.description, {
 			    
 	
