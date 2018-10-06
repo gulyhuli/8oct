ddaba546d71a0994d337d0f1672615b10d349fd9
gulyhuli <42315248+gulyhuli@users.noreply.github.com>
2018-10-06 15:10:51 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -78,9 +78,8 @@ bot.onText(/^\/[a-zA-Z]+$/, msg => {
     case '/start':
     case '/help':
       bot.sendMessage(id, helper.description).then(() => {
-	       bot.sendMessage(id, `Выберите пункт меню ⬆
-Акция!
-Получи 124 рублея`),
+	      
+	       bot.sendMessage(id, helper.description)), 
 		return bot.sendMessage(id, helper.description){
 			
 			    
