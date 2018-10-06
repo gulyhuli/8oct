d6a4ee3d8818e386fa208a95c84257db2bf93280
gulyhuli <42315248+gulyhuli@users.noreply.github.com>
2018-10-06 15:41:48 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -77,9 +77,9 @@ bot.onText(/^\/[a-zA-Z]+$/, msg => {
 
     case '/start':
     case '/help':
-	  bot.sendMessage(id, helper.description).then(() => {
+ bot.sendMessage(id, helper.description).then(() => {
         return bot.sendMessage(id, `Выберите пункт меню`, {
-		 return bot.sendMessage(id, `Выберите пункт меню`, {
+		  return bot.sendMessage(id, `Выберите пункт меню`, {
 			    
 	
           reply_markup: {
