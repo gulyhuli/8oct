227bb593997e5db808b3cb8003340cb641e87b2d
gulyhuli <42315248+gulyhuli@users.noreply.github.com>
2018-10-06 14:23:17 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ bot.onText(/^\/[a-zA-Z]+$/, msg => {
     case '/help':
       bot.sendMessage(id, helper.description).then(() => {
         return bot.sendMessage(id, `Выберите пункт меню`, {
-		return bot.sendMessage(id, `Выберите пункт меню`, {
+	
           reply_markup: {
             keyboard: keyboard.home,
             resize_keyboard: true
